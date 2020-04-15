9e17c9ca42a56453d677070fc18bafe3cc1d7b19
gal bar <galbar07@gmail.com>
2020-04-15 18:23:41 +0300

    adding success.ejs and css file

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const ejs= require("ejs");
 var pg = require('pg');
 require('dotenv').config();
 const bodyParser = require("body-parser");
-const client = require('twilio')(process.env.ACCOUNTSID, process.env.AUTHTOKEN);
+const accountSid = 'AC1466c4f4a98b5e5cf65842856a98fef1';
+const authToken = '89be4cf58ff12a12227c97f45725c27b';
+const client = require('twilio')(accountSid, authToken);
+var conString = "postgres://ydzqisno:rYRJefbRoE4czrr1H2UurqUtrSNkp7R3@balarama.db.elephantsql.com:5432/ydzqisno"; 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -28,7 +31,7 @@ var Last_Name;
 var Company;
 
 function Conect_SQL(){
-  var client = new pg.Client(process.env.POSTGRESS);
+  var client = new pg.Client(conString);
     client.connect(function(err) {
         if(err) {
           return console.error('could not connect to postgres', err);
@@ -46,8 +49,9 @@ function Conect_SQL(){
 
 };
 
+
 function Update_Heat(){
-  var client = new pg.Client(process.env.POSTGRESS);
+  var client = new pg.Client(conString);
   client.connect(function(err) {
     if(err) {
       return console.error('could not connect to postgres', err);
@@ -60,7 +64,6 @@ function Update_Heat(){
   });
 
 }
-
 function init(User){
   console.log(User);
   First_Name=User.firstname;
@@ -108,8 +111,8 @@ app.get("/form",function(req,res){
   res.render("Form",{First_Name,Id,Company,monthDateYear});
 });
 
-app.get("/sucess",function(req,res){
-  res.render("sucess");
+app.get("/success",function(req,res){
+  res.render("success");
 });
 
 
@@ -120,14 +123,13 @@ app.get("/failure",function(req,res){
 app.post("/fill",function(req,res){
   console.log(req.body);
 
-  if(req.body.confirm=="on" ){
-      res.redirect("sucess")
-  }
-  else{
-      res.redirect("failure");
-  }
+ 
+      res.redirect("success")
+  
+ 
 });
   
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
  Conect_SQL();
