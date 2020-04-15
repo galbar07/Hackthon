fbab8bf42a034e0446df7859079e79df9a4a2333
gal bar <galbar07@gmail.com>
2020-04-15 17:25:25 +0300

    Changes in css and automatic fill form

--- app.js
+++ app.js
@@ -2,78 +2,134 @@ const express = require("express");
 const ejs= require("ejs");
 var pg = require('pg');
 const bodyParser = require("body-parser");
-var fill_form=[];
+const accountSid = 'AC1466c4f4a98b5e5cf65842856a98fef1';
+const authToken = '89be4cf58ff12a12227c97f45725c27b';
+const client = require('twilio')(accountSid, authToken);
 var conString = "postgres://ydzqisno:rYRJefbRoE4czrr1H2UurqUtrSNkp7R3@balarama.db.elephantsql.com:5432/ydzqisno"; 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
+
 var argv = require('minimist')(process.argv.slice(2));//Extract Id from Command Line
 
 
-Id = argv["_"][0];
-Name = argv["_"][1];
-Last = argv["_"][2];
-FName = Name+" "+Last;
-
-
-app.get("/",function(req,res){
-    res.send("Hello");
-    
- });
-
- app.get("/form",function(req,res){
-    res.render("Form",{Id});
- });
-
- app.get("/sucess",function(req,res){
-    res.render("sucess");
- });
-
- 
- app.get("/failure",function(req,res){
-    res.render("failure");
- });
-
- app.post("/fill",function(req,res){
-    console.log(req.body);
-    if(req.body.confirm=="on" ){
-        res.redirect("sucess")
-    }
-    else{
-        res.redirect("failure");
-    }
- });
-
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
-    SQL_Withdrawl();
-  });
-
-
-  function SQL_Withdrawl(){
-    
-   var str = Conect_SQL();//Return Row
-   console.log(fill_form);
-   
-
-  }
+var Id = argv["_"][0];
+var Heat = parseFloat(argv["_"][1]);
+let phone;
+var First_Name;
+var Last_Name;
+var Company;
 
 function Conect_SQL(){
-    var client = new pg.Client(conString);
+  var client = new pg.Client(conString);
     client.connect(function(err) {
         if(err) {
           return console.error('could not connect to postgres', err);
         }
-        client.query('SELECT * FROM persons', function(err, result) {
+        client.query('SELECT * FROM employees where id='+Id, function(err, result) {
           if(err) {
             return console.error('error running query', err);
           }
-          console.log(result.rows);
-          client.end();
+          init(result.rows[0]);
+          Update_Heat();
+          
+         
         });
       });
 
 };
+
+function Update_Heat(){
+  var client = new pg.Client(conString);
+  client.connect(function(err) {
+    if(err) {
+      return console.error('could not connect to postgres', err);
+    }
+    client.query('UPDATE employees set heat='+Heat +"where id="+Id, function(err, result) {
+      if(err) {
+        return console.error('error running query', err);
+      }
+    });
+  });
+
+}
+
+function init(User){
+  console.log(User);
+  First_Name=User.firstname;
+  Last_Name=User.lastname;
+  phone=User.phone;
+  Company=User.company;
+  Check_heat();
+}
+
+
+function Check_heat(){
+
+
+// if(Heat>=38){
+
+//    client.messages
+//     .create({
+//     body: 'Hi'+First_Name +" "+ Last_Name +',Please go home',
+//     from: '+19382229581',
+//     to:'+972'+phone
+//  })
+// .then(message => console.log(message.sid));
+
+//   }
+//   else{
+//     client.messages
+// .create({
+//    body: 'Hi'+First_Name +" "+ Last_Name +', please fill the following form : http://localhost:3000/Form',
+//    from: '+19382229581',
+//    to:'+972'+phone
+//  })
+// .then(message => console.log(message.sid));
+//   }
+
+
+}
+
+
+
+
+app.get("/",function(req,res){
+  res.send("Hello");
   
+});
+
+app.get("/form",function(req,res){
+  res.render("Form",{First_Name,Id,Company});
+});
+
+app.get("/sucess",function(req,res){
+  res.render("sucess");
+});
+
+
+app.get("/failure",function(req,res){
+  res.render("failure");
+});
+
+app.post("/fill",function(req,res){
+  console.log(req.body);
+
+  if(req.body.confirm=="on" ){
+      res.redirect("sucess")
+  }
+  else{
+      res.redirect("failure");
+  }
+});
+  
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+  Conect_SQL();
+  
+
+});
+
+
